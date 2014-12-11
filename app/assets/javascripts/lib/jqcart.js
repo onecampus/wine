ab0e51c5ed2c54e27bed818635cce4f8ef2f03a3
flowerwrong <sysuyangkang@gmail.com>
2014-12-11 16:01:44 +0800

    beautify jqcart.js and comission.html.erb

--- app/assets/javascripts/lib/jqcart.js
+++ app/assets/javascripts/lib/jqcart.js
@@ -1,7 +1,7 @@
-(function () {
-  $(document).on('ready page:load', function () {
+(function() {
+  $(document).on('ready page:load', function() {
     var Utils = {
-      setParam: function (name, value) {
+      setParam: function(name, value) {
         $.localStorage.set(name, value);
       },
       getParam: function(name) {
@@ -25,25 +25,39 @@
       //向购物车中添加商品
       addProduct: function(product) {
         var shoppingCart = Utils.getParam("shoppingCart"),
-            JsonStr = Object;
-        if(shoppingCart === null || shoppingCart === "") {
+          JsonStr = Object;
+        if (shoppingCart === null || shoppingCart === "") {
           //第一次加入商品
-          JsonStr = {"productList": [{"id": product.id, "name": product.name, "num": product.num, "price": product.price}], "totalNumber": product.num, "totalAmount": (product.price * product.num)};
+          JsonStr = {
+            "productList": [{
+              "id": product.id,
+              "name": product.name,
+              "num": product.num,
+              "price": product.price
+            }],
+            "totalNumber": product.num,
+            "totalAmount": (product.price * product.num)
+          };
           Utils.setParam("shoppingCart", "'" + JSON.stringify(JsonStr));
         } else {
           JsonStr = JSON.parse(shoppingCart.substr(1, shoppingCart.length));
           var productList = JsonStr.productList;
           var result = false;
           //查找购物车中是否有该商品
-          for(var i in productList) {
-            if(productList[i].id == product.id) {
+          for (var i in productList) {
+            if (productList[i].id == product.id) {
               productList[i].num = parseInt(productList[i].num) + parseInt(product.num);
               result = true;
             }
           }
-          if(!result) {
+          if (!result) {
             //没有该商品就直接加进去
-            productList.push({"id": product.id, "name": product.name, "num": product.num, "price": product.price});
+            productList.push({
+              "id": product.id,
+              "name": product.name,
+              "num": product.num,
+              "price": product.price
+            });
           }
           //重新计算总价
           JsonStr.totalNumber = parseInt(JsonStr.totalNumber) + parseInt(product.num);
@@ -60,8 +74,8 @@
         var JsonStr = JSON.parse(shoppingCart.substr(1, shoppingCart.length));
         var productList = JsonStr.productList;
 
-        for(var i in productList){
-          if(productList[i].id == id) {
+        for (var i in productList) {
+          if (productList[i].id == id) {
             JsonStr.totalNumber = parseInt(JsonStr.totalNumber) + 1;
             JsonStr.totalAmount = parseFloat(JsonStr.totalAmount) + (1 * parseFloat(productList[i].price));
             productList[i].num = productList[i].num + 1;
@@ -79,8 +93,8 @@
         var JsonStr = JSON.parse(shoppingCart.substr(1, shoppingCart.length));
         var productList = JsonStr.productList;
 
-        for(var i in productList){
-          if(productList[i].id == id) {
+        for (var i in productList) {
+          if (productList[i].id == id) {
             JsonStr.totalNumber = parseInt(JsonStr.totalNumber) + (parseInt(num) - parseInt(productList[i].num));
             JsonStr.totalAmount = parseFloat(JsonStr.totalAmount) + ((parseInt(num) * parseFloat(productList[i].price)) - parseInt(productList[i].num) * parseFloat(productList[i].price));
             productList[i].num = parseInt(num);
@@ -107,8 +121,8 @@
         var JsonStr = JSON.parse(shoppingCart.substr(1, shoppingCart.length));
         var productList = JsonStr.productList;
         var result = false;
-        for(var i in productList) {
-          if(productList[i].id == product.id) {
+        for (var i in productList) {
+          if (productList[i].id == product.id) {
             result = true;
           }
         }
@@ -120,8 +134,8 @@
         var JsonStr = JSON.parse(shoppingCart.substr(1, shoppingCart.length));
         var productList = JsonStr.productList;
         var list = [];
-        for(var i in productList) {
-          if(productList[i].id == id) {
+        for (var i in productList) {
+          if (productList[i].id == id) {
             JsonStr.totalNumber = parseInt(JsonStr.totalNumber) - parseInt(productList[i].num);
             JsonStr.totalAmount = parseFloat(JsonStr.totalAmount) - parseInt(productList[i].num) * parseFloat(productList[i].price);
           } else {
@@ -142,26 +156,28 @@
       'num': 2,
       'price': 199.98
     };
+
+
     Cart.addProduct(product);
     var productList = Cart.getProductList(); // 取出购物车商品
     console.log(productList);
 
 
-    if(Cart.existProduct(1)) {
+    if (Cart.existProduct(1)) {
       Cart.incProductNum(1);
     }
     productList = Cart.getProductList(); // 取出购物车商品
     console.log(productList);
 
 
-    if(Cart.existProduct(1)) {
+    if (Cart.existProduct(1)) {
       Cart.updateProductNum(1, 40);
     }
     productList = Cart.getProductList(); // 取出购物车商品
     console.log(productList);
 
 
-    if(Cart.existProduct(1)) {
+    if (Cart.existProduct(1)) {
       Cart.deleteProduct(1);
     }
     productList = Cart.getProductList(); // 取出购物车商品
