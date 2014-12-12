d939a5d57e09f587e63ed65dcffce70d85c179d4
flowerwrong <sysuyangkang@gmail.com>
2014-12-12 19:10:54 +0800

    Fixed rake db:reset

--- app/models/vritualcard.rb
+++ app/models/vritualcard.rb
@@ -1,5 +1,5 @@
 class Vritualcard < ActiveRecord::Base
-  validates :user_id, :province, :city, :region, :address, :mobile, presence: true
+  validates :user_id, :money, presence: true
 
   belongs_to :user
 end
