93f33ab4f52e56df0ef09373e3f21c5d112bdd44
flowerwrong <sysuyangkang@gmail.com>
2014-12-18 23:29:11 +0800

    Fix register.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -12,7 +12,8 @@ class SiteController < CustomerController
                                                  :index_wait_receive,
                                                  :index_order_history,
                                                  :create_order,
-                                                 :big_whell_ajax]
+                                                 :big_whell_ajax,
+                                                 :test]
   skip_before_filter :verify_authenticity_token, only: [:create_order,
                                                         :big_wheel_ajax,
                                                         :scratch_off_ajax,
@@ -21,6 +22,10 @@ class SiteController < CustomerController
 
   def index
     @recommend_products = Product.last 8
+  end
+
+  def test
+
   end
 
   def index_cats
