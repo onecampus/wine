89d5138bfc11c6d0d1290d866bf6f2b74480f6d7
flowerwrong <sysuyangkang@gmail.com>
2014-12-19 15:41:39 +0800

    Fix production mail send config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,13 +76,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'thecampus.cc', port: 3000}
+  config.action_mailer.default_url_options = { host: '203.195.172.200', port: 80 }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'thecampus.cc',
+    domain:               '203.195.172.200',
     user_name:            'sysuyangkang@gmail.com',
     password:             '1*flower@wrong*1',
     authentication:       'plain',
