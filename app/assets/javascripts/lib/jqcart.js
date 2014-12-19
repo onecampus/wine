6deff25e4a8507359e3b40a203ef8bbb1908f835
gang <499801006@qq.com>
2014-12-19 17:31:36 +0800

    added show_product.html 立即购买按钮
    添加了立即购买功能,跳转到订单页面

--- app/assets/javascripts/lib/jqcart.js
+++ app/assets/javascripts/lib/jqcart.js
@@ -72,13 +72,67 @@ $(document).ready(function() {
         //保存购物车
         Utils.setParam("shoppingCart", "'" + JSON.stringify(JsonStr));
       }
+    },
+    buyProduct: function(product) {
+      var shoppingCart = Utils.getParam("shoppingCart"),
+      JsonStr = Object;
+      if (shoppingCart === null || shoppingCart === "") {
+        //第一次加入商品
+        JsonStr = {
+          "productList": [{
+            "id": product.id,
+            "name": product.name,
+            "englishname": product.englishname,
+            "num": product.num,
+            "price": product.price,
+            "img": product.img,
+            "buyMark": product.buyMark
+          }],
+          "totalNumber": product.num,
+          "totalAmount": (product.price * product.num)
+        };
+        Utils.setParam("shoppingCart", "'" + JSON.stringify(JsonStr));
+      } else {
+        JsonStr = JSON.parse(shoppingCart.substr(1, shoppingCart.length));
+        var productList = JsonStr.productList;
+        var result = false;
+        //查找购物车中是否有该商品
+        for (var i in productList) {
+          if (productList[i].id == product.id) {
+            productList[i].num = parseInt(productList[i].num) + parseInt(product.num);
+            productList[i].buyMark = true;
+            result = true;
+          }
+          else {
+            productList[i].buyMark = false;
+          }
+        }
+        if (!result) {
+          //没有该商品就直接加进去
+          productList.push({
+            "id": product.id,
+            "name": product.name,
+            "englishname": product.englishname,
+            "num": product.num,
+            "price": product.price,
+            "img": product.img,
+            "buyMark": product.buyMark
+          });
+        }
+        //重新计算总价
+        JsonStr.totalNumber = parseInt(JsonStr.totalNumber) + parseInt(product.num);
+        JsonStr.totalAmount = parseFloat(JsonStr.totalAmount) + (parseInt(product.num) * parseFloat(product.price));
+        OrderDetail.totalNumber = JsonStr.totalNumber;
+        OrderDetail.totalAmount = JsonStr.totalAmount;
+        //保存购物车
+        Utils.setParam("shoppingCart", "'" + JSON.stringify(JsonStr));
+      }
     }
   };
   /*
   加入购物车
   */
   $("#input-shoppingcart").click(function() {
-    console.log('it does work');
     var name = $(".product-name").text(),
         englishname = $(".product-englishname").text(),
         price = $(".price").text(),
@@ -86,7 +140,6 @@ $(document).ready(function() {
         id = $(".product-id").val(),
         img = $(".top-img").children().attr("src"),
         buyMark = false;
-    console.log(img); // /uploads/4108047cc89a7d6311ceb19150479e4e.jpg
     var product = {
       'id': id,
       'name': name,
@@ -98,6 +151,31 @@ $(document).ready(function() {
     };
     Cart.addProduct(product);
     showShoppingCartItem();
+  });
+
+  /*
+  立即购买
+  */
+  $(".buy-now").click(function(){
+    var name = $(".product-name").text(),
+    englishname = $(".product-englishname").text(),
+    price = $(".price").text(),
+    num = $(".p-num").val(),
+    id = $(".product-id").val(),
+    img = $(".top-img").children().attr("src"),
+    buyMark = true;
+    var product = {
+      'id': id,
+      'name': name,
+      'englishname': englishname,
+      'num': num,
+      'price': price,
+      'img': img,
+      'buyMark': buyMark
+    };
+    Cart.buyProduct(product);
+    showShoppingCartItem();
+    window.location = '/customer/orders/settlement';
   });
 });
 
