ad181c73405f2dbecc6e5ff3433485c6073f0068
huangbinbin <1158298762@qq.com>
2015-01-13 17:17:37 +0800

    add the kuaidi companies and the codes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,6 +46,50 @@ module ApplicationHelper
   def express_companies
     [
       {
+        code: 'ems',
+        company: 'ems快递'
+      },
+      {
+        code: 'shunfeng',
+        company: '顺丰'
+      },
+      {
+        code: 'shentong',
+        company: '申通'
+      },
+      {
+        code: 'yuantong',
+        company: '圆通速递'
+      },
+      {
+        code: 'zhaijisong',
+        company: '宅急送'
+      },
+      {
+        code: 'yafengsudi',
+        company: '亚风速递'
+      },
+      {
+        code: 'quanyikuaidi',
+        company: '全一快递'
+      },
+      {
+        code: 'tiantian',
+        company: '天天快递'
+      },
+      {
+        code: 'zhongtiekuaiyun',
+        company: '中铁快运'
+      },
+      {
+        code: 'yunda',
+        company: '韵达快运'
+      },
+      {
+        code: 'sue',
+        company: '速尔物流'
+      },
+      {
         code: 'aae',
         company: 'aae全球专递'
       },
@@ -110,166 +154,259 @@ module ApplicationHelper
         company: 'fedex（国外）'
       },
       {
-        code: 'ems',
-        company: 'ems快递'
-      },
-      {
         code: 'feikangda',
         company: '飞康达物流'
       },
+      {
+        code: 'fenghuangkuaidi',
+        company: '凤凰快递'
+      },
+      {
+        code: 'feikuaida',
+        company: '飞快达'
+      },
+      {
+        code: 'guotongkuaidi',
+        company: '国通快递'
+      },
+      {
+        code: 'ganzhongnengda',
+        company: '港中能达物流'
+      },
+      {
+        code: 'guangdongyouzhengwuliu',
+        company: '广东邮政物流'
+      },
+      {
+        code: 'gongsuda',
+        company: '共速达'
+      },
+      {
+        code: 'huitongkuaidi',
+        company: '汇通快运'
+      },
+      {
+        code: 'hengluwuliu',
+        company: '恒路物流'
+      },
+      {
+        code: 'huaxialongwuliu',
+        company: '华夏龙物流'
+      },
+      {
+        code: 'haihongwangsong',
+        company: '海红'
+      },
+      {
+        code: 'haiwaihuanqiu',
+        company: '海外环球'
+      },
+      {
+        code: 'jiayiwuliu',
+        company: '佳怡物流'
+      },
+      {
+        code: 'jinguangsudikuaijian',
+        company: '京广速递'
+      },
+      {
+        code: 'jixianda',
+        company: '急先达'
+      },
+      {
+        code: 'jjwl',
+        company: '佳吉物流'
+      },
+      {
+        code: 'jymwl',
+        company: '加运美物流'
+      },
+      {
+        code: 'jindawuliu',
+        company: '金大物流'
+      },
+      {
+        code: 'jialidatong',
+        company: '嘉里大通'
+      },
+      {
+        code: 'jykd',
+        company: '晋越快递'
+      },
+      {
+        code: 'kuaijiesudi',
+        company: '快捷速递'
+      },
+      {
+        code: 'lianb',
+        company: '联邦快递（国内）'
+      },
+      {
+        code: 'lianhaowuliu',
+        company: '联昊通物流'
+      },
+      {
+        code: 'longbanwuliu',
+        company: '龙邦物流'
+      },
+      {
+        code: 'lijisong',
+        company: '立即送'
+      },
+      {
+        code: 'lejiedi',
+        company: '乐捷递'
+      },
+      {
+        code: 'minghangkuaidi',
+        company: '民航快递'
+      },
+      {
+        code: 'meiguokuaidi',
+        company: '美国快递'
+      },
+      {
+        code: 'menduimen',
+        company: '门对门'
+      },
+      {
+        code: 'ocs',
+        company: 'OCS'
+      },
+      {
+        code: 'peisihuoyunkuaidi',
+        company: '配思货运'
+      },
+      {
+        code: 'quanchenkuaidi',
+        company: '全晨快递'
+      },
+      {
+        code: 'quanfengkuaidi',
+        company: '全峰快递'
+      },
+      {
+        code: 'quanjitong',
+        company: '全际通物流'
+      },
+      {
+        code: 'quanritongkuaidi',
+        company: '全日通快递'
+      },
+      {
+        code: 'rufengda',
+        company: '如风达'
+      },
+      {
+        code: 'santaisudi',
+        company: '三态速递'
+      },
+      {
+        code: 'shenghuiwuliu',
+        company: '盛辉物流'
+      },
+      {
+        code: 'shengfeng',
+        company: '盛丰物流'
+      },
+      {
+        code: 'saiaodi',
+        company: '赛澳递'
+      },
+      {
+        code: 'tiandihuayu',
+        company: '天地华宇'
+      },
+      {
+        code: 'tnt',
+        company: 'tnt'
+      },
+      {
+        code: 'ups',
+        company: 'ups'
+      },
+      {
+        code: 'wanjiawuliu',
+        company: '万家物流'
+      },
+      {
+        code: 'wenjiesudi',
+        company: '文捷航空速递'
+      },
+      {
+        code: 'wuyuan',
+        company: '伍圆'
+      },
+      {
+        code: 'wxwl',
+        company: '万象物流'
+      },
+      {
+        code: 'xinfengwuliu',
+        company: '信丰物流'
+      },
+      {
+        code: 'yibangwuliu',
+        company: '一邦速递'
+      },
+      {
+        code: 'youshuwuliu',
+        company: '优速物流'
+      },
+      {
+        code: 'youzhengguonei',
+        company: '邮政包裹挂号信'
+      },
+      {
+        code: 'youzhengguoji',
+        company: '邮政国际包裹挂号信'
+      },
+      {
+        code: 'yuanchengwuliu',
+        company: '远成物流'
+      },
+      {
+        code: 'yuanweifeng',
+        company: '源伟丰快递'
+      },
+      {
+        code: 'yuanzhijiecheng',
+        company: '元智捷诚快递'
+      },
+      {
+        code: 'yuntongkuaidi',
+        company: '运通快递'
+      },
+      {
+        code: 'yuefengwuliu',
+        company: '越丰物流'
+      },
+      {
+        code: 'yad',
+        company: '源安达'
+      },
+      {
+        code: 'yinjiesudi',
+        company: '银捷速递'
+      },
+      {
+        code: 'zhongtong',
+        company: '中通速递'
+      },
+      {
+        code: 'zhongyouwuliu',
+        company: '中邮物流'
+      },
+      {
+        code: 'zhongxinda',
+        company: '忠信达'
+      },
+      {
+        code: 'zhimakaimen',
+        company: '芝麻开门'
+      }
     ]
   end
-
-
-  feikangda
-  飞康达物流
-  fenghuangkuaidi
-  凤凰快递
-  feikuaida
-  飞快达
-  guotongkuaidi
-  国通快递
-  ganzhongnengda
-  港中能达物流
-  guangdongyouzhengwuliu
-  广东邮政物流
-  gongsuda
-  共速达
-  huitongkuaidi
-  汇通快运
-  hengluwuliu
-  恒路物流
-  huaxialongwuliu
-  华夏龙物流
-  haihongwangsong
-  海红
-  haiwaihuanqiu
-  海外环球
-  jiayiwuliu
-  佳怡物流
-  jinguangsudikuaijian
-  京广速递
-  jixianda
-  急先达
-  jjwl
-  佳吉物流
-  jymwl
-  加运美物流
-  jindawuliu
-  金大物流
-  jialidatong
-  嘉里大通
-  jykd
-  晋越快递
-  kuaijiesudi
-  快捷速递
-  lianb
-  联邦快递（国内）
-  lianhaowuliu
-  联昊通物流
-  longbanwuliu
-  龙邦物流
-  lijisong
-  立即送
-  lejiedi
-  乐捷递
-  minghangkuaidi
-  民航快递
-  meiguokuaidi
-  美国快递
-  menduimen
-  门对门
-  ocs
-  OCS
-  peisihuoyunkuaidi
-  配思货运
-  quanchenkuaidi
-  全晨快递
-  quanfengkuaidi
-  全峰快递
-  quanjitong
-  全际通物流
-  quanritongkuaidi
-  全日通快递
-  quanyikuaidi
-  全一快递
-  rufengda
-  如风达
-  santaisudi
-  三态速递
-  shenghuiwuliu
-  盛辉物流
-  shentong
-  申通
-  shunfeng
-  顺丰
-  sue
-  速尔物流
-  shengfeng
-  盛丰物流
-  saiaodi
-  赛澳递
-  tiandihuayu
-  天地华宇
-  tiantian
-  天天快递
-  tnt
-  tnt
-  ups
-  ups
-  wanjiawuliu
-  万家物流
-  wenjiesudi
-  文捷航空速递
-  wuyuan
-  伍圆
-  wxwl
-  万象物流
-  xinbangwuliu
-  新邦物流
-  xinfengwuliu
-  信丰物流
-  yafengsudi
-  亚风速递
-  yibangwuliu
-  一邦速递
-  youshuwuliu
-  优速物流
-  youzhengguonei
-  邮政包裹挂号信
-  youzhengguoji
-  邮政国际包裹挂号信
-  yuanchengwuliu
-  远成物流
-  yuantong
-  圆通速递
-  yuanweifeng
-  源伟丰快递
-  yuanzhijiecheng
-  元智捷诚快递
-  yunda
-  韵达快运
-  yuntongkuaidi
-  运通快递
-  yuefengwuliu
-  越丰物流
-  yad
-  源安达
-  yinjiesudi
-  银捷速递
-  zhaijisong
-  宅急送
-  zhongtiekuaiyun
-  中铁快运
-  zhongtong
-  中通速递
-  zhongyouwuliu
-  中邮物流
-  zhongxinda
-  忠信达
-  zhimakaimen
-  芝麻开门
-
 
   def country_to_select
     [
