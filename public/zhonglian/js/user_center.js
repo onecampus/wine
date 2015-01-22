66fb6b36adec9ffc872f29646398e25a163250ff
huangbinbin <1158298762@qq.com>
2015-01-22 09:39:00 +0800

    set the images size in the usercenter pages

--- public/zhonglian/js/user_center.js
+++ public/zhonglian/js/user_center.js
@@ -1,16 +1,15 @@
 // 判断有无商品交易记录
 $(document).ready(function() {
-	if (!$('.top').next().size()) {
-		$('.main').append('<div class="announcement"><p>暂无记录</p></div>');
-	} else {
-		var $img = $("img.left");
-		var width = $img.width();
-		$img.height(width);
-		$(
-			window).resize(function() {
-			var $img = $("img.left");
-			var width = $img.width();
-			$img.height(width);
-		});
-	}
+  if (!$('.top').next().size()) {
+    $('.main').append('<div class="announcement"><p>暂无记录</p></div>');
+  } else {
+    var $img = $("img.left");
+    var width = $img.width();
+    $img.height(width);
+    $(window).resize(function() {
+      var $img = $("img.left");
+      var width = $img.width();
+      $img.height(width);
+    });
+  }
 });
