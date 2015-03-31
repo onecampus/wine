bd00b218d67ca02d1baaecb49eec3bf1118bc3bc
flowerwrong <sysuyangkang@gmail.com>
2015-03-31 11:30:44 +0800

    Add integral to cal.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -120,15 +120,24 @@ class OrdersController < ApplicationController
         buyer_profile = buyer.profile  # 当前用户的额外信息
 
         # 之前有没有购买过东西, 如果没有, 那么是第一次购买, 生成 invite_code
-        old_orders = buyer.orders.blank?
-        generate_invite_code_or_not(old_orders, buyer_profile)
+        old_orders_flag = buyer.orders.blank?
+        generate_invite_code_or_not(old_orders_flag, buyer_profile)
 
         commissioner = nil  # 第一级提成者
         total_price = @order.total_price  # 总价
+
         # 购买积分计算百分比
         product_score_percent = SiteConfig.where(key: 'product_score_percent', config_type: 'commission_config').first.val
 
         total_price = total_price.to_f  # 提成金额
+
+        # 用戶積分結算
+        if total_price > 100
+          integral = buyer.integral
+          integral.amount = (total_price % 100).to_i
+          integral.save!
+        end
+
         if @order.order_type == '普通订单'
           @order.products.each do |p|
             total_price -= p.price if p.is_commission == 0
@@ -146,14 +155,16 @@ class OrdersController < ApplicationController
         score = buyer.score
         commission_price = total_price * product_score_percent.to_f  # 提成金额
         commission_mark = commission_price.to_i
-        score.mark += commission_mark
-        score.remain_mark += commission_mark
+        p '=' * 20
+        p commission_mark
+        score.mark = score.mark.to_i + commission_mark  # 总分
+        score.remain_mark += commission_mark  # 保留分数
         score.save!
 
         remain_mark_old = score.remain_mark
+        p remain_mark_old
 
         if remain_mark_old >= 100  # 如果购买者分数超过100，就计算提成
-
           # 对分数进行求余
           remain_mark = remain_mark_old % 100
           commission_score = remain_mark_old - remain_mark
@@ -175,8 +186,8 @@ class OrdersController < ApplicationController
             Rails.logger.info "commissioner is #{commissioner.username}"
             vritualcard = commissioner.vritualcard  # 会员卡
             commissioner_money = vritualcard.money.to_f  # 会员卡金额
-            c_o_commission_a = SiteConfig.where(key: 'c_o_commission_a').first.val
-            commission_money = commission_score * c_o_commission_a
+            c_o_commission_a = SiteConfig.where(key: 'current_order_commission_a').first.val
+            commission_money = commission_score * c_o_commission_a.to_f
             commissioner_money += commission_money
             vritualcard.money = commissioner_money.round(2)
             vritualcard.save!
@@ -188,8 +199,8 @@ class OrdersController < ApplicationController
               Rails.logger.info "commissioner_p is #{commissioner_p.username}"
               vritualcard_p = commissioner_p.vritualcard
               commissioner_money_p = vritualcard_p.money.to_f
-              c_o_commission_b = SiteConfig.where(key: 'c_o_commission_b').first.val
-              commission_money_p = commission_score * c_o_commission_b
+              c_o_commission_b = SiteConfig.where(key: 'current_order_commission_b').first.val
+              commission_money_p = commission_score * c_o_commission_b.to_f
               commissioner_money_p += commission_money_p
               vritualcard_p.money = commissioner_money_p.round(2)
               vritualcard_p.save!
@@ -201,8 +212,8 @@ class OrdersController < ApplicationController
                 Rails.logger.info "commissioner_p_p is #{commissioner_p_p.username}"
                 vritualcard_p_p = commissioner_p_p.vritualcard
                 commissioner_money_p_p = vritualcard_p_p.money.to_f
-                c_o_commission_c = SiteConfig.where(key: 'c_o_commission_c').first.val
-                commission_money_p_p = commission_score * c_o_commission_c
+                c_o_commission_c = SiteConfig.where(key: 'current_order_commission_c').first.val
+                commission_money_p_p = commission_score * c_o_commission_c.to_f
                 commissioner_money_p_p += commission_money_p_p
                 vritualcard_p_p.money = commissioner_money_p_p.round(2)
                 vritualcard_p_p.save!
@@ -270,9 +281,9 @@ class OrdersController < ApplicationController
   end
 
   # 之前有没有购买过东西, 如果没有, 那么是第一次购买, 生成 invite_codes
-  def generate_invite_code_or_not(old_orders, current_user_profile)
-    Rails.logger.info "old_orders is #{old_orders}"
-    if old_orders
+  def generate_invite_code_or_not(old_orders_flag, current_user_profile)
+    Rails.logger.info "old_orders_flag is #{old_orders_flag}"
+    if old_orders_flag
       invite_code = User.generate_invite_code
       current_user_profile.invite_code = invite_code
       current_user_profile.save!
